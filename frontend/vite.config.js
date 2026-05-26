18b41269d2746e330cc4a0eb87633fcda8a2cd88
Raktimmaity <raktimmaity2003@gmail.com>
2026-05-26 13:19:32 +0530

    update colors 1

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
-import legacy from '@vitejs/plugin-legacy';
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    legacy({
-      targets: ['defaults', 'chrome >= 64'],
-      modernPolyfills: true,
-    }),
   ],
   base: '/',
   server: {
